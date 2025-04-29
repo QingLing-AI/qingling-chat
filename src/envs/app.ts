752b445f849700b9974e77ce50b1ae103ce6b866
wenhua <lshgdut@qq.com>
2025-04-29 15:39:40 +0800

    feat: 增加QINGLING_CUSTOMIZED标记

--- src/envs/app.ts
+++ src/envs/app.ts
@@ -33,6 +33,7 @@ export const getAppConfig = () => {
   return createEnv({
     client: {
       NEXT_PUBLIC_ENABLE_SENTRY: z.boolean(),
+      NEXT_PUBLIC_QINGLING_CUSTOMIZED: z.boolean(),
     },
     server: {
       ACCESS_CODES: z.any(z.string()).optional(),
@@ -57,6 +58,8 @@ export const getAppConfig = () => {
       SSRF_ALLOW_PRIVATE_IP_ADDRESS: z.boolean().optional(),
       SSRF_ALLOW_IP_ADDRESS_LIST: z.string().optional(),
       MARKET_BASE_URL: z.string().optional(),
+
+      QINGLING_CUSTOMIZED: z.boolean(),
     },
     runtimeEnv: {
       // Sentry
@@ -90,6 +93,9 @@ export const getAppConfig = () => {
       SSRF_ALLOW_PRIVATE_IP_ADDRESS: process.env.SSRF_ALLOW_PRIVATE_IP_ADDRESS === '1',
       SSRF_ALLOW_IP_ADDRESS_LIST: process.env.SSRF_ALLOW_IP_ADDRESS_LIST,
       MARKET_BASE_URL: process.env.MARKET_BASE_URL,
+
+      NEXT_PUBLIC_QINGLING_CUSTOMIZED: process.env.QINGLING_CUSTOMIZED !== '0',
+      QINGLING_CUSTOMIZED: process.env.QINGLING_CUSTOMIZED !== '0',
     },
   });
 };
