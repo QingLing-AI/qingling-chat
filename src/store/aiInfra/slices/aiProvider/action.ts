lshgdut/lobe-chat

feat(auth): 优化登录界面显示逻辑
- 根据 isQinglingCustomized 标志显示不同的登录按钮文本
- 更新 AI 模型和提供商的启用逻辑，支持定制化版本
- 在中间件中添加对 /chat 路由的保护

src/store/aiInfra/slices/aiProvider/action.ts
@@ -3,7 +3,7 @@ import { SWRResponse, mutate } from 'swr';
 import { StateCreator } from 'zustand/vanilla';
 
 import { DEFAULT_MODEL_PROVIDER_LIST } from '@/config/modelProviders';
-import { isDeprecatedEdition, isDesktop, isUsePgliteDB } from '@/const/version';
+import { isDeprecatedEdition, isDesktop, isUsePgliteDB, isQinglingCustomized } from '@/const/version';
 import { useClientDataSWR } from '@/libs/swr';
 import { aiProviderService } from '@/services/aiProvider';
 import { AiInfraStore } from '@/store/aiInfra/store';
@@ -176,8 +176,9 @@ export const createAiProviderSlice: StateCreator<
 
         const { LOBE_DEFAULT_MODEL_LIST } = await import('@/config/aiModels');
         return {
-          enabledAiModels: LOBE_DEFAULT_MODEL_LIST.filter((m) => m.enabled),
-          enabledAiProviders: DEFAULT_MODEL_PROVIDER_LIST.filter(
+          // NOTE(lsh): 未登录时不显示任何模型
+          enabledAiModels: isQinglingCustomized ? [] : LOBE_DEFAULT_MODEL_LIST.filter((m) => m.enabled),
+          enabledAiProviders: isQinglingCustomized ? [] : DEFAULT_MODEL_PROVIDER_LIST.filter(
             (provider) => provider.enabled,
           ).map((item) => ({ id: item.id, name: item.name, source: 'builtin' })),
           runtimeConfig: {},

QingLing-AI/qingling-chat

src/store/aiInfra/slices/aiProvider/action.ts
@@ -2,7 +2,7 @@ import { uniqBy } from 'lodash-es';
 import { SWRResponse, mutate } from 'swr';
 import { StateCreator } from 'zustand/vanilla';
 
-import { isDeprecatedEdition, isDesktop, isUsePgliteDB } from '@/const/version';
+import { isDeprecatedEdition, isDesktop, isUsePgliteDB, isQinglingCustomized } from '@/const/version';
 import { useClientDataSWR } from '@/libs/swr';
 import { aiProviderService } from '@/services/aiProvider';
 import { AiInfraStore } from '@/store/aiInfra/store';
@@ -214,14 +214,15 @@ export const createAiProviderSlice: StateCreator<
         ).map((item) => ({ id: item.id, name: item.name, source: 'builtin' }));
         return {
           builtinAiModelList,
-          enabledAiModels: builtinAiModelList.filter((m) => m.enabled),
-          enabledAiProviders: enabledAiProviders,
-          enabledChatAiProviders: enabledAiProviders.filter((provider) => {
+          // NOTE(lsh): 未登录时不显示任何模型
+          enabledAiModels: isQinglingCustomized ? [] : builtinAiModelList.filter((m) => m.enabled),
+          enabledAiProviders: isQinglingCustomized ? [] : enabledAiProviders,
+          enabledChatAiProviders: isQinglingCustomized ? [] : enabledAiProviders.filter((provider) => {
             return builtinAiModelList.some(
               (model) => model.providerId === provider.id && model.type === 'chat',
             );
           }),
-          enabledImageAiProviders: enabledAiProviders
+          enabledImageAiProviders: isQinglingCustomized ? [] : enabledAiProviders
             .filter((provider) => {
               return builtinAiModelList.some(
                 (model) => model.providerId === provider.id && model.type === 'image',