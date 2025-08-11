lshgdut/lobe-chat

refactor(app): 移除清岭定制相关代码
- 从 appConfig 中移除 QINGLING_CUSTOMIZED 配置项
- 使用环境变量直接判断是否为清岭定制版本
- 调整 featureFlags、auth、version 等模块中的相关逻辑
- 优化 JWT_SECRET_KEY 根据版本动态设置
- 调整内置工具的显示逻辑
- 更新相关测试用例

src/const/auth.ts
@@ -1,3 +1,4 @@
+import { isQinglingCustomized } from '@/const/version'
 import { authEnv } from '@/config/auth';
 
 export const enableClerk = authEnv.NEXT_PUBLIC_ENABLE_CLERK_AUTH;
@@ -8,7 +9,7 @@ export const LOBE_CHAT_AUTH_HEADER = 'X-lobe-chat-auth';
 
 export const OAUTH_AUTHORIZED = 'X-oauth-authorized';
 
-export const JWT_SECRET_KEY = 'qing!ling@qingling';
+export const JWT_SECRET_KEY = isQinglingCustomized ? 'qing!ling@qingling' : 'LobeHub · LobeChat';
 export const NON_HTTP_PREFIX = 'http_nosafe';
 
 /* eslint-disable typescript-sort-keys/interface */

QingLing-AI/qingling-chat

src/const/auth.ts
@@ -1,3 +1,4 @@
+import { isQinglingCustomized } from '@/const/version'
 import { authEnv } from '@/config/auth';
 
 export const enableClerk = authEnv.NEXT_PUBLIC_ENABLE_CLERK_AUTH;
@@ -9,7 +10,7 @@ export const LOBE_CHAT_OIDC_AUTH_HEADER = 'Oidc-Auth';
 
 export const OAUTH_AUTHORIZED = 'X-oauth-authorized';
 
-export const SECRET_XOR_KEY = 'qing!ling@qingling';
+export const SECRET_XOR_KEY = isQinglingCustomized ? 'qing!ling@qingling' : 'LobeHub · LobeChat';
 
 /* eslint-disable typescript-sort-keys/interface */
 export interface ClientSecretPayload {