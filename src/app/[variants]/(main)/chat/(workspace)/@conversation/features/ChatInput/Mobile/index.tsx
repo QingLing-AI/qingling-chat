lshgdut/lobe-chat

feat(ChatInput): add customized actions for Qingling
- Introduce appEnv.NEXT_PUBLIC_QINGLING_CUSTOMIZED to determine action buttons
- Add new action keys for Qingling customized version:
  - knowledgeBase
  - tools
- Remove some action keys for Qingling customized version:
  - history
  - params
  - mainToken
- Update leftActions in both Desktop and Mobile components

src/app/[variants]/(main)/chat/(workspace)/@conversation/features/ChatInput/Mobile/index.tsx
@@ -7,6 +7,7 @@ import { memo, useRef, useState } from 'react';
 import { Flexbox } from 'react-layout-kit';
 import STT from 'src/features/ChatInput/ActionBar/STT';
 
+import { appEnv } from '@/config/app'
 import ActionBar from '@/features/ChatInput/ActionBar';
 import { ActionKeys } from '@/features/ChatInput/ActionBar/config';
 import SaveTopic from '@/features/ChatInput/Topic';
@@ -19,7 +20,16 @@ import Files from './Files';
 import InputArea from './InputArea';
 import SendButton from './Send';
 
-const defaultLeftActions: ActionKeys[] = [
+const defaultLeftActions: ActionKeys[] = appEnv.NEXT_PUBLIC_QINGLING_CUSTOMIZED ? [
+  'model',
+  'search',
+  'fileUpload',
+  'knowledgeBase',
+  // 'history',
+  'tools',
+  // 'params',
+  // 'mainToken',
+] : [
   'model',
   'search',
   'fileUpload',

QingLing-AI/qingling-chat

src/app/[variants]/(main)/chat/(workspace)/@conversation/features/ChatInput/Mobile/index.tsx
@@ -11,12 +11,22 @@ import {
   MobileChatInput as ChatInput,
   ChatInputProvider,
 } from '@/features/ChatInput';
+import { appEnv } from '@/envs/app'
 import { useChatStore } from '@/store/chat';
 import { aiChatSelectors } from '@/store/chat/slices/aiChat/selectors';
 
 import { useSend } from '../useSend';
 
-const leftActions: ActionKeys[] = [
+const leftActions: ActionKeys[] = appEnv.NEXT_PUBLIC_QINGLING_CUSTOMIZED ? [
+  'model',
+  'search',
+  'fileUpload',
+  'knowledgeBase',
+  'tools',
+  '---',
+  [/* 'params', 'history',  */'stt', 'clear'],
+  // 'mainToken',
+] : [
   'model',
   'search',
   'fileUpload',