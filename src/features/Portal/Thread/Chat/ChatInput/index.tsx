lshgdut/lobe-chat

feat(featureFlags): 更新语音识别功能配置并调整相关组件
- 在 featureFlags 配置中将 speech_to_text 设置为 false
- 更新 MobileChatInput、AssistantActionsBar 和 Desktop 组件，根据 enableSTT 状态控制语音识别功能的显示

src/features/Portal/Thread/Chat/ChatInput/index.tsx
@@ -9,6 +9,7 @@ import { ActionKeys } from '@/features/ChatInput/ActionBar/config';
 import DesktopChatInput, { FooterRender } from '@/features/ChatInput/Desktop';
 import { useGlobalStore } from '@/store/global';
 import { systemStatusSelectors } from '@/store/global/selectors';
+import { featureFlagsSelectors, useServerConfigStore } from '@/store/serverConfig';
 
 import Footer from './Footer';
 import TextArea from './TextArea';
@@ -26,6 +27,8 @@ const Desktop = memo(() => {
     systemStatusSelectors.systemStatus(s).hideThreadLimitAlert,
     s.updateSystemStatus,
   ]);
+
+  const { enableSTT } = useServerConfigStore(featureFlagsSelectors);
 
   return (
     <>
@@ -52,7 +55,7 @@ const Desktop = memo(() => {
       )}
       <DesktopChatInput
         inputHeight={inputHeight}
-        leftActions={leftActions}
+        leftActions={enableSTT ? leftActions : leftActions.filter((action) => action !== 'stt')}
         onInputHeightChange={(height) => {
           updateSystemStatus({ threadInputHeight: height });
         }}

QingLing-AI/qingling-chat

src/features/Portal/Thread/Chat/ChatInput/index.tsx
@@ -11,6 +11,7 @@ import WideScreenContainer from '@/features/Conversation/components/WideScreenCo
 import { useChatStore } from '@/store/chat';
 import { useGlobalStore } from '@/store/global';
 import { systemStatusSelectors } from '@/store/global/selectors';
+import { featureFlagsSelectors, useServerConfigStore } from '@/store/serverConfig';
 
 import { useSendThreadMessage } from './useSend';
 
@@ -23,6 +24,7 @@ const Desktop = memo(() => {
   ]);
 
   const { send, disabled, generating, stop } = useSendThreadMessage();
+  const { enableSTT } = useServerConfigStore(featureFlagsSelectors);
 
   return (
     <WideScreenContainer>
@@ -54,7 +56,7 @@ const Desktop = memo(() => {
           if (!instance) return;
           useChatStore.setState({ threadInputEditor: instance });
         }}
-        leftActions={threadActions}
+        leftActions={enableSTT ? threadActions : threadActions.filter((action) => action !== 'stt')}
         onSend={() => {
           send();
         }}