lshgdut/lobe-chat

refact: 重构qinglingCustomized的判断方式

src/app/[variants]/(main)/settings/common/index.tsx
@@ -1,9 +1,9 @@
-import { serverFeatureFlags } from '@/config/featureFlags';
+import { appEnv } from '@/config/app'
 import Common from './features/Common';
 import Theme from './features/Theme';
 
 const Page = () => {
-  const qinglingCustomized = serverFeatureFlags().qinglingCustomized
+  const qinglingCustomized = appEnv.NEXT_PUBLIC_QINGLING_CUSTOMIZED
   return (
     <>
       <Theme />

QingLing-AI/qingling-chat

src/app/[variants]/(main)/settings/common/index.tsx
@@ -1,10 +1,10 @@
 import Appearance from './features/Appearance';
 import ChatAppearance from './features/ChatAppearance';
-import { serverFeatureFlags } from '@/config/featureFlags';
+import { appEnv } from '@/envs/app'
 import Common from './features/Common/Common';
 
 const Page = () => {
-  const qinglingCustomized = serverFeatureFlags().qinglingCustomized
+  const qinglingCustomized = appEnv.NEXT_PUBLIC_QINGLING_CUSTOMIZED
   return (
     <>
       {!qinglingCustomized && <Common />}