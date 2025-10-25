lshgdut/lobe-chat

refactor: 重构清岭定制相关逻辑
- 移除 appEnv 中的 QINGLING_CUSTOMIZED 相关配置
- 在 serverConfig 中添加 isQinglingCustomized 配置项
- 更新相关组件，使用新的 isQinglingCustomized 配置
- 调整 ChatInput、SessionList 等组件的定制逻辑
- 优化代码结构，提高可维护性

src/app/[variants]/(auth)/next-auth/signin/AuthSignInBox.tsx
@@ -10,15 +10,14 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { appEnv } from '@/config/app';
 import { BRANDING_NAME } from '@/const/branding';
 import CustomLogo from '@/components/Branding/ProductLogo/Custom';
 import BrandWatermark from '@/components/BrandWatermark';
 import AuthIcons from '@/components/NextAuth/AuthIcons';
 import { DOCUMENTS_REFER_URL, PRIVACY_URL, TERMS_URL } from '@/const/url';
+import { useServerConfigStore } from '@/store/serverConfig';
 import { useUserStore } from '@/store/user';
 
-const QINGLING_CUSTOMIZED = appEnv.NEXT_PUBLIC_QINGLING_CUSTOMIZED;
 
 const { Title, Paragraph } = Typography;
 
@@ -80,6 +79,8 @@ export default memo(() => {
 
   const oAuthSSOProviders = useUserStore((s) => s.oAuthSSOProviders);
 
+  const { isQinglingCustomized } = useServerConfigStore((s)=>s.serverConfig)
+
   const searchParams = useSearchParams();
 
   // Redirect back to the page url
@@ -103,7 +104,7 @@ export default memo(() => {
     }
   };
 
-  const footerBtns = QINGLING_CUSTOMIZED ? [
+  const footerBtns = isQinglingCustomized ? [
     { href: TERMS_URL, id: 0, label: t('footerPageLink__terms') },
   ] : [
     { href: DOCUMENTS_REFER_URL, id: 0, label: t('footerPageLink__help') },
@@ -120,7 +121,7 @@ export default memo(() => {
           <div className={styles.text}>
             <Title className={styles.title} level={4}>
               <div>
-                {QINGLING_CUSTOMIZED ? <CustomLogo size={48}/> : <LobeChat size={48} />}
+                {isQinglingCustomized ? <CustomLogo size={48}/> : <LobeChat size={48} />}
               </div>
               {t('signIn.start.title', { applicationName: BRANDING_NAME })}
             </Title>

src/tools/web-browsing/index.ts
@@ -1,10 +1,12 @@
 import dayjs from 'dayjs';
 
-import { isQinglingCustomized } from '@/config/app';
+import { appEnv } from '@/config/app';
 import { BuiltinToolManifest } from '@/types/tool';
 
 import { systemPrompt } from './systemRole';
 import { systemPrompt as systemPromptCustomized } from './systemRoleCustomized';
+
+const isQinglingCustomized = appEnv.NEXT_PUBLIC_QINGLING_CUSTOMIZED;
 
 export const WebBrowsingApiName = {
   crawlMultiPages: 'crawlMultiPages',

QingLing-AI/qingling-chat

src/app/[variants]/(auth)/next-auth/signin/AuthSignInBox.tsx
@@ -11,13 +11,12 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import { memo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { appEnv } from '@/envs/app';
 import CustomLogo from '@/components/Branding/ProductLogo/Custom';
 import BrandWatermark from '@/components/BrandWatermark';
 import AuthIcons from '@/components/NextAuth/AuthIcons';
+import { useServerConfigStore } from '@/store/serverConfig';
 import { useUserStore } from '@/store/user';
 
-const QINGLING_CUSTOMIZED = appEnv.NEXT_PUBLIC_QINGLING_CUSTOMIZED;
 
 const useStyles = createStyles(({ css, token }) => ({
   button: css`
@@ -78,6 +77,8 @@ export default memo(() => {
 
   const oAuthSSOProviders = useUserStore((s) => s.oAuthSSOProviders);
 
+  const { isQinglingCustomized } = useServerConfigStore((s)=>s.serverConfig)
+
   const searchParams = useSearchParams();
 
   // Redirect back to the page url, fallback to '/' if failed
@@ -103,7 +104,7 @@ export default memo(() => {
     }
   };
 
-  const footerBtns = QINGLING_CUSTOMIZED ? [
+  const footerBtns = isQinglingCustomized ? [
     { href: TERMS_URL, id: 0, label: t('footerPageLink__terms') },
   ] : [
     { href: DOCUMENTS_REFER_URL, id: 0, label: t('footerPageLink__help') },
@@ -120,7 +121,7 @@ export default memo(() => {
           <div className={styles.text}>
             <Text as={'h4'} className={styles.title}>
               <div>
-                {QINGLING_CUSTOMIZED ? <CustomLogo size={48}/> : <LobeHub size={48} />}
+                {isQinglingCustomized ? <CustomLogo size={48}/> : <LobeHub size={48} />}
               </div>
               {t('signIn.start.title', { applicationName: BRANDING_NAME })}
             </Text>

src/tools/web-browsing/index.ts
@@ -1,10 +1,12 @@
 import { BuiltinToolManifest } from '@lobechat/types';
 import dayjs from 'dayjs';
 
-import { isQinglingCustomized } from '@/envs/app';
+import { appEnv } from '@/envs/app';
 
 import { systemPrompt } from './systemRole';
 import { systemPrompt as systemPromptCustomized } from './systemRoleCustomized';
+
+const isQinglingCustomized = appEnv.NEXT_PUBLIC_QINGLING_CUSTOMIZED;
 
 export const WebBrowsingApiName = {
   crawlMultiPages: 'crawlMultiPages',