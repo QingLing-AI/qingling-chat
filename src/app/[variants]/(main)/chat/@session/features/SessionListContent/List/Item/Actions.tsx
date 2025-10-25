lshgdut/lobe-chat

feat(chat): conditionally display delete button based on app environment
- Add import for appEnv configuration
- Implement conditional rendering of delete button in chat session list
- Hide delete button for customized app environments

src/app/[variants]/(main)/chat/@session/features/SessionListContent/List/Item/Actions.tsx
@@ -17,6 +17,7 @@ import {
 import { memo, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import { appEnv } from '@/config/app';
 import { isServerMode } from '@/const/version';
 import { configService } from '@/services/config';
 import { useSessionStore } from '@/store/session';
@@ -146,7 +147,8 @@ const Actions = memo<ActionProps>(({ group, id, openCreateGroupModal, setOpen })
                 key: 'export',
                 label: t('export', { ns: 'common' }),
               },
-          {
+              // NOTE(lsh): 定制模式下不显示删除按钮
+          (appEnv.NEXT_PUBLIC_QINGLING_CUSTOMIZED || appEnv.QINGLING_CUSTOMIZED) ? undefined : {
             danger: true,
             icon: <Icon icon={Trash} />,
             key: 'delete',

QingLing-AI/qingling-chat

src/app/[variants]/(main)/chat/@session/features/SessionListContent/List/Item/Actions.tsx
@@ -18,6 +18,7 @@ import {
 import { memo, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
+import { appEnv } from '@/envs/app';
 import { isDesktop, isServerMode } from '@/const/version';
 import { configService } from '@/services/config';
 import { useGlobalStore } from '@/store/global';
@@ -171,7 +172,8 @@ const Actions = memo<ActionProps>(({ group, id, openCreateGroupModal, parentType
                 key: 'export',
                 label: t('export', { ns: 'common' }),
               },
-          {
+              // NOTE(lsh): 定制模式下不显示删除按钮
+          (appEnv.NEXT_PUBLIC_QINGLING_CUSTOMIZED || appEnv.QINGLING_CUSTOMIZED) ? undefined : {
             danger: true,
             icon: <Icon icon={Trash} />,
             key: 'delete',