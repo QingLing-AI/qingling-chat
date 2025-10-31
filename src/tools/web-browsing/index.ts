lshgdut/lobe-chat

feat(web-browsing): 适配定制版本联网提示词
- 在 web-browsing 工具中添加了定制版本联网提示词的适配
- 新增了系统提示和搜索引擎列表的定制化版本
- 根据配置决定使用定制化版本还是默认版本

src/tools/web-browsing/index.ts
@@ -1,8 +1,10 @@
 import dayjs from 'dayjs';
 
+import { isQinglingCustomized } from '@/config/app';
 import { BuiltinToolManifest } from '@/types/tool';
 
 import { systemPrompt } from './systemRole';
+import { systemPrompt as systemPromptCustomized } from './systemRoleCustomized';
 
 export const WebBrowsingApiName = {
   crawlMultiPages: 'crawlMultiPages',
@@ -39,7 +41,13 @@ export const WebBrowsingManifest: BuiltinToolManifest = {
           searchEngines: {
             description: 'The search engines you can use:',
             items: {
-              enum: [
+              enum: isQinglingCustomized ? [
+                'google',
+                'google scholar',
+                'bilibili',
+                'bing',
+                'baidu',
+              ] : [
                 'google',
                 'bilibili',
                 'bing',
@@ -112,6 +120,8 @@ export const WebBrowsingManifest: BuiltinToolManifest = {
     avatar: '🌐',
     title: 'Web Browsing',
   },
-  systemRole: systemPrompt(dayjs(new Date()).format('YYYY-MM-DD')),
+  systemRole: (isQinglingCustomized ? systemPromptCustomized : systemPrompt)(
+    dayjs(new Date()).format('YYYY-MM-DD')
+  ),
   type: 'builtin',
 };

QingLing-AI/qingling-chat

src/tools/web-browsing/index.ts
@@ -1,7 +1,10 @@
 import { BuiltinToolManifest } from '@lobechat/types';
 import dayjs from 'dayjs';
 
+import { isQinglingCustomized } from '@/envs/app';
+
 import { systemPrompt } from './systemRole';
+import { systemPrompt as systemPromptCustomized } from './systemRoleCustomized';
 
 export const WebBrowsingApiName = {
   crawlMultiPages: 'crawlMultiPages',
@@ -32,7 +35,13 @@ export const WebBrowsingManifest: BuiltinToolManifest = {
           searchEngines: {
             description: 'The search engines you can use:',
             items: {
-              enum: [
+              enum: isQinglingCustomized ? [
+                'google',
+                'google scholar',
+                'bilibili',
+                'bing',
+                'baidu',
+              ] : [
                 'google',
                 'bilibili',
                 'bing',
@@ -105,6 +114,8 @@ export const WebBrowsingManifest: BuiltinToolManifest = {
     avatar: '🌐',
     title: 'Web Browsing',
   },
-  systemRole: systemPrompt(dayjs(new Date()).format('YYYY-MM-DD')),
+  systemRole: (isQinglingCustomized ? systemPromptCustomized : systemPrompt)(
+    dayjs(new Date()).format('YYYY-MM-DD')
+  ),
   type: 'builtin',
 };