lshgdut/lobe-chat

fix: 调整默认的 jwt secret

src/const/auth.ts
@@ -8,7 +8,7 @@ export const LOBE_CHAT_AUTH_HEADER = 'X-lobe-chat-auth';
 
 export const OAUTH_AUTHORIZED = 'X-oauth-authorized';
 
-export const JWT_SECRET_KEY = 'LobeHub · LobeChat';
+export const JWT_SECRET_KEY = 'qing!ling@qingling';
 export const NON_HTTP_PREFIX = 'http_nosafe';
 
 /* eslint-disable typescript-sort-keys/interface */

QingLing-AI/qingling-chat

packages/const/src/auth.ts
@@ -7,4 +7,4 @@ export const LOBE_CHAT_OIDC_AUTH_HEADER = 'Oidc-Auth';
 
 export const OAUTH_AUTHORIZED = 'X-oauth-authorized';
 
-export const SECRET_XOR_KEY = 'LobeHub · LobeHub';
+export const SECRET_XOR_KEY = 'qing!ling@qingling';